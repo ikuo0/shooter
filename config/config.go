23562fd344d3eaa8ef954a3ea9fa98b8c2124d69
ikuo hayasaka <ikuo.pg@gmail.com>
2018-10-19 21:55:19 +0900

    append

---
+++ config/config.go
@@ -0,0 +1,59 @@
+
+package config
+
+import (
+	"encodign/json"
+	"log"
+	"os"
+)
+
+
+type StKeyConfig struct {
+	Valid bool `json:"Valid"`
+	AxisUp int `json:"AxisUp"`
+	AxisRight int `json:"AxisRight"`
+	AxisDown int `json:"AxisDown"`
+	AxisLeft int `json:"AxisLeft"`
+	ButtonShot int `json:"ButtonShot"`
+	ButtonBoost int `json:"ButtonBoost"`
+	ButtonLock int `json:"ButtonLock"`
+}
+
+var KeyConfig StKeyConfig{};
+
+func Read(filename string) {
+	bytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Print(err)
+	} else {
+		if err := json.Unmarshal(bytes, &KeyConfig); err != nil {
+			log.Print(err)
+		}
+	}
+}
+
+func Write(filename string) {
+	jsonBytes, err := json.Marshal(KeyConfig)
+	if err != nil {
+	} else {
+		ioutil.writeFile(filename, jsonBytes)
+	}
+}
+
+func Dump() {
+    v := reflect.Indirect(reflect.ValueOf(KeyConfig))
+    t := v.Type()
+
+    for i := 0; i < t.NumField(); i++ {
+        println("Field: " + t.Field(i).Name)
+
+        f := v.Field(i)
+        i := f.Interface()
+        if value, ok := i.(int); ok {
+            println("Value: " + strconv.Itoa(value))
+        } else {
+            println("Value: " + f.String())
+        }
+    }
+}
+
---
+++ config/config_test.go
@@ -0,0 +1,13 @@
+
+package main
+
+import (
+	"testing"
+	"fmt"
+	"github.com/ikuo0/shooter/config"
+)
+
+func TestAll(t *testing.T) {
+	fmt.Println("TEST")
+}
+
